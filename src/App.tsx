b5106860a78bb4e9fdee6a405a1ccdd70cd33baa
Andriy Nikorych <andrei,nikorich@gmail.com>
2021-09-29 20:29:35 +0300

    create APP without date

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getWeather } from './api/api';
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
-
 export const App: React.FC = () => {
+  const [city, setCity] = useState<string>('Chernivtsi');
+  // const [date, setDate] = useState<number>(0);
+  const [currentWeather, setcurrentWeather] = useState<Weather | null>(null);
+
+  const loadWeather = async () => {
+    const weatherFromApi = await getWeather(city);
+
+    setcurrentWeather(weatherFromApi);
+  };
+
+  const hanleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    loadWeather();
+    setCity('');
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCity(event.target.value);
+  };
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <form
+            className="find-movie"
+            onSubmit={hanleSubmit}
+          >
+            <input
+              type="text"
+              className="search-field"
+              placeholder="Choose city"
+              value={city}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+
+        {currentWeather && (
+          <div>
+            <h2>{currentWeather.name}</h2>
+            <h3 className="info info__temp">
+              {Math.round(currentWeather.main.temp)}
+              &#176;C
+            </h3>
+            <ul>
+              <li>{`High/Low: ${currentWeather.main.temp_max}/${currentWeather.main.temp_min}`}</li>
+              <li>{`Pressure: ${currentWeather.main.pressure} mb`}</li>
+              <li>{`Humidity: ${currentWeather.main.humidity}%`}</li>
+              <li>{`Visibility: ${currentWeather.visibility} m`}</li>
+              <li>{`Wind: ${currentWeather.wind.speed} km/h`}</li>
+            </ul>
+
+          </div>
+        )}
+      </main>
     </div>
   );
 };
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,27 @@
 /// <reference types="react-scripts" />
+
+type Weather = {
+  main: Main,
+  sys: Sys,
+  name: string,
+  weather: Array<string>,
+  visibility: string,
+  wind: Wind,
+};
+
+type Main = {
+  temp: number,
+  temp_min: number,
+  temp_max: number,
+  pressure: number,
+  humidity: number,
+};
+
+type Sys = {
+  country: string,
+};
+
+type Wind = {
+  speed: number,
+  deg: number,
+};
